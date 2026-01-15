a23f4e20f7a0be9251dc46d4275e07f5d6d030fb
rail5 <andrew@rail5.org>
2026-01-15 20:33:44 +0800

    Clear old decorations, do not stack

--- src/languages/yaccComponentNumbers.ts
+++ src/languages/yaccComponentNumbers.ts
@@ -1,18 +1,23 @@
 import * as vscode from 'vscode';
 import { YACCDocument, NodeType } from './parser/yaccParser';
 
+const componentNumberDecorationType = vscode.window.createTextEditorDecorationType({});
+
 export function decorateComponentNumbers(editor: vscode.TextEditor, yaccDoc: YACCDocument) {
-    if (editor.document.languageId !== 'yacc') return;
+    if (editor.document.languageId !== 'yacc') {
+        // Clear any old decorations if this editor is not yacc anymore.
+        editor.setDecorations(componentNumberDecorationType, []);
+        return;
+    }
 
     const decorations: vscode.DecorationOptions[] = [];
-    const decoType = vscode.window.createTextEditorDecorationType({});
 
     for (const node of yaccDoc.nodes) {
         if (node.nodeType !== NodeType.Rule) continue;
 
         const rhsComponents = yaccDoc.components
             .filter(c => c.offset >= node.offset && c.end <= node.end)
-            .sort((a, b) => a.offset - b.offset); // be explicit
+            .sort((a, b) => a.offset - b.offset);
 
         let count = 1;
         for (const comp of rhsComponents) {
@@ -21,21 +26,21 @@ export function decorateComponentNumbers(editor: vscode.TextEditor, yaccDoc: YAC
                 continue;
             }
 
-			decorations.push({
-				range: new vscode.Range(
-					editor.document.positionAt(comp.offset),
-					editor.document.positionAt(comp.offset)
-				),
-				renderOptions: {
-					before: {
-						contentText: `\$${count++}:`,
-						color: '#888',
-						margin: '0 3px 0 0'
-					}
-				}
-			});
+            decorations.push({
+                range: new vscode.Range(
+                    editor.document.positionAt(comp.offset),
+                    editor.document.positionAt(comp.offset)
+                ),
+                renderOptions: {
+                    before: {
+                        contentText: `\$${count++}:`,
+                        color: '#888',
+                        margin: '0 3px 0 0'
+                    }
+                }
+            });
         }
     }
 
-    editor.setDecorations(decoType, decorations);
+    editor.setDecorations(componentNumberDecorationType, decorations);
 }
